b3fb637833fce3e8d16e30ed36dea40ef6838584
mikaelvs785-star <mikaelvs785@gmail.com>
2026-09-15 08:30:06 -0300

    feat(frontend): conecta roteamento e sessao protegida

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
-import LoginPage from './pages/LoginPage'
+import { BrowserRouter } from 'react-router-dom'
+import AuthProvider from './auth/AuthProvider'
+import AppRoutes from './routes/AppRoutes'
+import './routes/routes.css'
 
 export default function App() {
-  return <LoginPage />
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </BrowserRouter>
+  )
 }
