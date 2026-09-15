cdf372519f64900b093c9b75ca1717c114c2ec84
mikaelvs785-star <mikaelvs785@gmail.com>
2026-09-15 14:00:07 -0300

    feat: connect protected teacher community creation route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -51,7 +51,7 @@ export default function AppRoutes() {
           <Route path="/professor/jornal/:noticiaId" element={<NewsDetailPage />} />
           <Route path="/professor/jornal/nova" element={<CreateNewsPage />} />
           <Route path="/professor/comunidades" element={<TeacherCommunitiesPage />} />
-          <Route path="/professor/comunidades/nova" element={page('Criar comunidade', 'Cadastro de uma nova comunidade.')} />
+          <Route path="/professor/comunidades/nova" element={<DirectorCreateCommunityPage />} />
           <Route path="/professor/comunidades/:comunidadeId" element={page('Comunidade', 'Conteúdo e administração da comunidade.')} />
           <Route path="/professor/comunidades/:comunidadeId/editar" element={page('Editar comunidade', 'Configurações da comunidade do professor.')} />
           <Route path="/professor/perfil" element={page('Meu perfil', 'Dados do professor autenticado.')} />
